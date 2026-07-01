844ebf47d8814e82f9f77eff1fcae4c78a838d13
malayakeys32-rgb <malayakeys32@gmail.com>
2026-06-30 18:55:48 -0700

    Delete routes/routes/.auth.js

--- routes/routes/.auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/login', (req, res) => {
-  res.json({ message: 'Auth route working' });
-});
-
-module.exports = router;
